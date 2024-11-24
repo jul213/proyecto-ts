cf6da3b8c74c1ba04e4903b45603d290c7b23c27
jul213 <158296107+jul213@users.noreply.github.com>
2024-11-24 09:18:56 +0000

    creacion de bucles for-in y for clasico

--- hola.ts
+++ hola.ts
@@ -9,6 +9,13 @@ enum listatareas {
 let lista = ["hola","sudo","ls","cd"];
 
 lista.forEach((elemento) => {
-    console.log(elemento);
+    console.log(elemento); //foreach
 });
 
+for(const elemento in lista){
+    console.log(elemento);  //for-in
+}
+
+for(let index = 0; index < lista.length; index++){
+    const elemento = lista[index]; //for clasico en ts
+}
