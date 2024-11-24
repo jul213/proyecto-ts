82f4e8be8fc7074456a94724ecd60047ee69fc66
jul213 <158296107+jul213@users.noreply.github.com>
2024-11-24 09:32:10 +0000

    creacion de while

--- hola.ts
+++ hola.ts
@@ -20,3 +20,15 @@ for(let index = 0; index < lista.length; index++){
     const elemento = lista[index]; //for clasico en ts
     console.log(`el indice de la lista actual ${index} y el elemento de la lista es ${elemento}`);
 }
+
+const numero: number = 6;
+let indice: number= 0;
+
+while(indice < numero){
+    if (indice == 6){
+    console.log("el indice a llegado a igualar a el numero");
+    } else{
+        console.log("el numero no a llegado a igualar el numero");
+        indice++;
+    }
+}
