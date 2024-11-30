6c78b62c2822b6a89ff717314dd1e26fe8f621cd
jul213 <158296107+jul213@users.noreply.github.com>
2024-11-30 12:30:54 +0000

    localstorage  uso

--- persistencia.ts
+++ persistencia.ts
@@ -3,3 +3,14 @@
 // 1. localstorage --> almacena los datos en el navegador (no se eliminan automaticamente)
 //2. sessionStorage --> la diferencia radica en la sesion de navegador. es decir, los datos se   persisten en la sesion de navegador
 //3. cookies --> tienen una fecha de caducidad y tambien tienen un ambito de URL
+
+//localStorage
+
+function usoLocalStorage(): void{
+
+    localStorage.setItem("nombre", "juan");
+}
+
+function lectura(): void{
+    localStorage.getItem("nombre");
+}
